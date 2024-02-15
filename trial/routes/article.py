9bbcf20fde889e75f9d7609decf82e2e289f7c24
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-16 01:09:19 +0545

    articleRoute

--- trial/routes/article.py
+++ trial/routes/article.py
@@ -1,2 +1,12 @@
 from typing import List
 from fastapi import APIRouter, HTTPException, status, Depends
+#api router is for routering, httpexcetion is to create exception or show error in your code to the user, status, to set status code and depends is used for security(authentication and authorization)
+
+from fastapi.responses import JSONResponse
+
+from sqlalchemy.orm import Session
+
+from schemas.article import ShowArticle, CreateArticle
+
+from models.article import Article
+
