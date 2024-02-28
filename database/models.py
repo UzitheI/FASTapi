0bc1c792d24a4bf1091353c3176d92d887671ebd
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-02-28 16:36:23 +0545

    sqlModels

--- database/models.py
+++ database/models.py
@@ -13,6 +13,8 @@ class User(Base):
     is_active=Column(Boolean,default=True)
 
     items=relationship('Item',back_populates='owner')
+    #every class is basically a seperate table, the relationship functon allows to build a communication with 2 different tables, here the items is related to owner, which basically means that every item has an owner or multiple owners, for user, every user can own an item or multiple items, this is how we create a sqlalchemy model, now this needs to communicate with the pydantic model for it to be effectively used by our web application
+    
 
 class Item(Base):
     __tablename__='items'
